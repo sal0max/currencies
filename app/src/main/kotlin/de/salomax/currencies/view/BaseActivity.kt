0496b7a564f73d3c51aa54af4fcd9bf434d239ff
Max <msal.coding@gmail.com>
2021-12-16 03:14:44 +0100

    better way to "transparent navigation bar"

--- app/src/main/kotlin/de/salomax/currencies/view/BaseActivity.kt
+++ app/src/main/kotlin/de/salomax/currencies/view/BaseActivity.kt
@@ -1,13 +1,8 @@
 package de.salomax.currencies.view
 
-import android.content.Context
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.WindowInsetsControllerCompat
-import com.google.android.material.color.MaterialColors
 import de.salomax.currencies.R
 import de.salomax.currencies.repository.Database
 
@@ -31,21 +26,7 @@ abstract class BaseActivity : AppCompatActivity() {
             }
         )
 
-        // "transparent" navigation bar
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            window.navigationBarColor = MaterialColors.getColor(this, R.attr.colorBackground, Color.BLACK)
-            WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars = booleanFromAttribute(R.attr.isLightTheme)
-            WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = booleanFromAttribute(R.attr.isLightTheme)
-        }
-
         super.onCreate(savedInstanceState)
-    }
-
-    private fun Context.booleanFromAttribute(attribute: Int): Boolean {
-        val attributes = obtainStyledAttributes(intArrayOf(attribute))
-        val dimension = attributes.getBoolean(0, false)
-        attributes.recycle()
-        return dimension
     }
 
 }
