7260c9dce731e73b5e3f00a8aeb07e64f710c915
Maximilian Salomon <msal.coding@gmail.com>
2022-04-16 03:28:13 +0200

    removed everything except translations

--- app/src/main/kotlin/de/salomax/currencies/util/DimenUtils.kt
+++
@@ -1,9 +0,0 @@
-package de.salomax.currencies.util
-
-import android.content.res.Resources
-
-@Suppress("unused")
-fun Float.pxToDp(): Float = (this / Resources.getSystem().displayMetrics.density)
-
-@Suppress("unused")
-fun Float.dpToPx(): Float = (this * Resources.getSystem().displayMetrics.density)
