141f3d3ba9da3dfa820bbc45099c665c87cf705c
Maximilian Salomon <msal.coding@gmail.com>
2023-02-12 02:16:23 +0100

    Don't scrape for localized currency names, but use the Java dictionary

--- helpers/src/main/kotlin/de/salomax/helpers/currencies/CurrencyFetcher.kt
+++ helpers/src/main/kotlin/de/salomax/helpers/currencies/CurrencyFetcher.kt
@@ -2,14 +2,11 @@
 
 package de.salomax.helpers.currencies
 
-import org.jsoup.Jsoup
 import java.util.*
 
 // !! change this to the target language !!
-private const val TARGET_LANGUAGE = "he"
+private val targetLanguage = Locale("zh", "CN")
 
-private const val USER_AGENT =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"
 private val ISO_NAMES = listOf(
     "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT",
     "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BZD",
@@ -28,137 +25,20 @@ private val ISO_NAMES = listOf(
 )
 
 fun main() {
+    println(
+        """
+            <?xml version="1.0" encoding="utf-8"?>
+            <resources>
+        """.trimIndent()
+    )
     for (isoName in ISO_NAMES) {
-        val iso = isoName.lowercase()
-
-        // bad results; don't use!
-//        getLocaleStringFromExchangeRatesCom(TARGET_LANGUAGE, iso)
-
-        val s0 = getLocaleStringFromInvestingCom(TARGET_LANGUAGE, iso)
-        val s1 = getWiseCom(TARGET_LANGUAGE, iso)
-        val s2 = getCoinmillCom(TARGET_LANGUAGE, iso)
-        val s3 = getMataffNet(TARGET_LANGUAGE, iso)
-
-        val hits = listOf(s0, s1, s2, s3)
-            .filterNot { it.isNullOrEmpty() }
-        val s = hits
-                .groupingBy { it }
-                .eachCount()
-                .filter {
-                    // good enough: 3/4
-                    if (hits.size == 4)
-                        it.value >= 3
-                    // good enough: 2/3, 2/2
-                    else if (hits.size >= 2)
-                        it.value >= 2
-                    else
-                        false
-                }
-                .keys.let { if (it.isEmpty()) null else it.first() }
-
-        if (s != null)
-            println("""<string name="name_$iso">$s</string>""")
-        else
-            println()
-
-        Thread.sleep(1200)
+        try {
+            val name = Currency.getInstance(isoName).getDisplayName(targetLanguage)
+                .replaceFirstChar { if (it.isLowerCase()) it.titlecase(targetLanguage) else it.toString() }
+            println("""    <string name="name_${isoName.lowercase()}">$name</string>""")
+        } catch (e: java.lang.IllegalArgumentException) {
+            println(isoName)
+        }
     }
-}
-
-fun getLocaleStringFromInvestingCom(targetLanguage: String, iso4217Alpha: String): String? {
-    return try {
-        val doc = Jsoup.connect(
-            "https://$targetLanguage.investing.com/currencies/" +
-                    "eur-${iso4217Alpha.lowercase()}"
-        )
-            .userAgent(USER_AGENT)
-            .get()
-        doc.selectFirst("[data-test=instrument-bottom-secondary-value]")
-            ?.text()
-            ?.uppercase()
-    } catch (e: Exception) {
-        null
-    }
-}
-
-fun getLocaleStringFromExchangeRatesCom(targetLanguage: String, iso4217Alpha: String): String? {
-    return try {
-        val doc = Jsoup.connect(
-            "https://www.exchange-rates.com/${targetLanguage.lowercase()}/" +
-                    "EUR/${iso4217Alpha.uppercase()}"
-        )
-            .userAgent(USER_AGENT)
-            .get()
-        val result = doc.selectFirst("h1")
-            ?.text()
-            ?.replace("Euro \\w+ ".toRegex(), "")
-            ?.replace(" Växelkurs", "")
-            ?.replace(" Kurs wymiany", "")
-            ?.uppercase()
-        if (result == "Exchange Rates and Currency Converter") null else result
-    } catch (e: Exception) {
-        null
-
-    }
-}
-
-fun getWiseCom(targetLanguage: String, iso4217Alpha: String): String? {
-    return try {
-        val doc = Jsoup.connect(
-            "https://wise.com/${targetLanguage.lowercase()}/" +
-                    "currency-converter/" +
-                    "eur-to-${iso4217Alpha.lowercase()}-rate"
-        )
-            .userAgent(USER_AGENT)
-            .get()
-        doc.selectFirst("th[colspan]")
-            ?.text()
-            ?.substringAfter("/ ")
-            ?.uppercase()
-    } catch (e: Exception) {
-        null
-    }
-}
-
-fun getCoinmillCom(targetLanguage: String, iso4217Alpha: String): String? {
-    return try {
-        val doc = Jsoup.connect(
-            "https://${targetLanguage.lowercase()}.coinmill.com/" +
-                    "${iso4217Alpha.lowercase()}_EUR"
-        )
-            .userAgent(USER_AGENT)
-            .get()
-        doc.selectFirst("#currencyBox0 > a")
-            ?.text()
-            ?.substringBefore(" (")
-            ?.uppercase()
-    } catch (e: Exception) {
-        null
-    }
-}
-
-fun getMataffNet(targetLanguage: String, iso4217Alpha: String): String? {
-    return try {
-        val doc = Jsoup.connect(
-            "https://www.mataf.net/" +
-                    "${targetLanguage.lowercase()}/" +
-                    "currency/" +
-                    "converter-EUR-${iso4217Alpha.uppercase()}"
-        )
-            .userAgent(USER_AGENT)
-            .get()
-        doc.selectFirst(".caption-helper")
-            ?.text()
-            ?.substringAfter("/ ")
-            ?.uppercase()
-    } catch (e: Exception) {
-        null
-    }
-}
-
-private fun String?.uppercase() = this?.replaceFirstChar {
-    if (it.isLowerCase())
-        it.titlecase(Locale.forLanguageTag(TARGET_LANGUAGE))
-    else
-        it.toString()
+    println("</resources>\n")
 }
