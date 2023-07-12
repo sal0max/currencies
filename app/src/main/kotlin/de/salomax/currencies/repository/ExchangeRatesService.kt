86bfe0942b3b9814167465edcc7ada8bd26b6f7d
Maximilian Salomon <msal.coding@gmail.com>
2023-07-12 02:47:27 +0200

    fixed wrong timeline data with InforEuro API

--- app/src/main/kotlin/de/salomax/currencies/repository/ExchangeRatesService.kt
+++ app/src/main/kotlin/de/salomax/currencies/repository/ExchangeRatesService.kt
@@ -11,6 +11,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import de.salomax.currencies.model.ApiProvider
 import de.salomax.currencies.model.Currency
 import de.salomax.currencies.model.ExchangeRates
+import de.salomax.currencies.model.Rate
 import de.salomax.currencies.model.Timeline
 import java.time.LocalDate
 import java.time.ZoneOffset
@@ -22,11 +23,15 @@ object ExchangeRatesService {
     /**
      * Get all the current exchange rates from the given api provider. Base will be Euro.
      */
-    suspend fun getRates(apiProvider: ApiProvider, date: LocalDate? = null): Result<ExchangeRates, FuelError> {
+    suspend fun getRates(
+        apiProvider: ApiProvider,
+        date: LocalDate? = null
+    ): Result<ExchangeRates, FuelError> {
         // Currency conversions are done relatively to each other - so it basically doesn't matter
         // which base is used here. However, Euro is a strong currency, preventing rounding errors.
         val base = Currency.EUR
-        val dateString = if (date != null) date.format(DateTimeFormatter.ISO_LOCAL_DATE) else "latest"
+        val dateString =
+            if (date != null) date.format(DateTimeFormatter.ISO_LOCAL_DATE) else "latest"
 
         return Fuel.get(
             when (apiProvider) {
@@ -34,12 +39,15 @@ object ExchangeRatesService {
                         "/$dateString" +
                         "?base=$base" +
                         "&v=${UUID.randomUUID()}"
+
                 ApiProvider.FRANKFURTER_APP -> apiProvider.baseUrl +
                         "/$dateString" +
                         "?base=$base"
+
                 ApiProvider.FER_EE -> apiProvider.baseUrl +
                         "/$dateString" +
                         "?base=$base"
+
                 ApiProvider.INFOR_EURO -> apiProvider.baseUrl +
                         "/monthly-rates" +
                         if (date != null) "?year=${date.year}" + "&month=${date.monthValue}"
@@ -70,7 +78,11 @@ object ExchangeRatesService {
      * Won't get all the symbols, as it makes a big difference in transferred data size:
      * ~12 KB for one symbol to ~840 KB for all symbols
      */
-    suspend fun getTimeline(apiProvider: ApiProvider, base: Currency, symbol: Currency): Result<Timeline, FuelError> {
+    suspend fun getTimeline(
+        apiProvider: ApiProvider,
+        base: Currency,
+        symbol: Currency
+    ): Result<Timeline, FuelError> {
         val endDate = LocalDate.now()
         val startDate = endDate.minusYears(1)
 
@@ -78,8 +90,51 @@ object ExchangeRatesService {
         // can't search for FOK - have to use DKK instead
         val parameterBase = if (base == Currency.FOK) "DKK" else base.iso4217Alpha()
         val parameterSymbol = if (symbol == Currency.FOK) "DKK" else symbol.iso4217Alpha()
-        // call api
-        return Fuel.get(
+
+        // CALL API
+
+        // InforEuro needs 2 calls: the API only provides EUR <-> symbol, without changing the base.
+        // So, we make 2 calls: EUR <-> base & EUR <-> symbol
+        return if (apiProvider == ApiProvider.INFOR_EURO) {
+            val deserializer = moshiDeserializerOf(
+                Moshi.Builder()
+                    .addLast(KotlinJsonAdapterFactory())
+                    .add(InforEuroTimelineAdapter(startDate, endDate))
+                    .build()
+                    .adapter(Timeline::class.java)
+            )
+            // EUR <-> base
+            val resultBase = Fuel.get(
+                "${apiProvider.baseUrl}/currencies/$parameterBase"
+            ).awaitResult(deserializer)
+            // error
+            if (resultBase.component2() != null) return resultBase
+
+            // EUR <-> symbol
+            val resultSymbol = Fuel.get(
+                "${apiProvider.baseUrl}/currencies/$parameterSymbol"
+            ).awaitResult(deserializer)
+            // error
+            if (resultSymbol.component2() != null) return resultSymbol
+
+            // success
+            val timeline = resultBase.get().copy(
+                provider = apiProvider,
+                base = (if (base == Currency.FOK) Currency.FOK else base).iso4217Alpha(),
+                rates = resultSymbol.get().rates?.map { symbolEntry ->
+                    val baseValue = resultBase.get().rates?.get(symbolEntry.key)
+                    Pair(
+                        symbolEntry.key,
+                        Rate(symbol, symbolEntry.value.value.div(baseValue?.value ?: 1f))
+                    )
+                }?.toMap()
+            )
+            Result.of { timeline }
+        }
+
+        // take care of the "normal" APIs
+        else Fuel.get(
+            @Suppress("KotlinConstantConditions")
             when (apiProvider) {
                 ApiProvider.EXCHANGERATE_HOST -> "${apiProvider.baseUrl}/timeseries" +
                         "?base=$parameterBase" +
@@ -87,34 +142,31 @@ object ExchangeRatesService {
                         "&start_date=${startDate.format(dateFormatter)}" +
                         "&end_date=${endDate.format(dateFormatter)}" +
                         "&symbols=$parameterSymbol"
+
                 ApiProvider.FRANKFURTER_APP -> "${apiProvider.baseUrl}/" +
                         startDate.format(dateFormatter) +
                         ".." +
                         endDate.format(dateFormatter) +
                         "?base=$parameterBase" +
                         "&symbols=$parameterSymbol"
+
                 ApiProvider.FER_EE -> "${apiProvider.baseUrl}/" +
                         startDate.format(dateFormatter) +
                         ".." +
                         endDate.format(dateFormatter) +
                         "?base=$parameterBase" +
                         "&symbols=$parameterSymbol"
-                ApiProvider.INFOR_EURO -> "${apiProvider.baseUrl}/" +
-                        "currencies/" +
-                        parameterSymbol
+
+                ApiProvider.INFOR_EURO -> TODO() // we can ignore this: never reachable
             }
         ).awaitResult(
             moshiDeserializerOf(
                 Moshi.Builder()
                     .addLast(KotlinJsonAdapterFactory())
                     .apply {
-                        if (apiProvider == ApiProvider.INFOR_EURO) {
-                            add(InforEuroTimelineAdapter(startDate, endDate))
-                        } else {
-                            add(RatesAdapter(base))
-                            add(LocalDateAdapter())
-                            add(TimelineRatesAdapter(symbol))
-                        }
+                        add(RatesAdapter(base))
+                        add(LocalDateAdapter())
+                        add(TimelineRatesAdapter(symbol))
                     }
                     .build()
                     .adapter(Timeline::class.java)
