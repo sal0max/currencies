b8ecf8ee8a23021c791cfbea44723ba12d40b251
Maximilian Salomon <msal.coding@gmail.com>
2023-11-12 11:34:13 +0100

    updated dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     // dependency-update-checker
     id("com.github.ben-manes.versions") version "0.49.0"
