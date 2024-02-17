8428bc5f4f7ffd5fb474d73e0440ccc37b849a3e
Maximilian Salomon <msal.coding@gmail.com>
2024-02-17 13:28:56 +0100

    prepare next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 26
         targetSdk = 34
         // SemVer
-        versionName = "1.21.3"
-        versionCode = 12103
+        versionName = "1.22.0"
+        versionCode = 12200
         archivesName.set("$applicationId-v$versionCode")
     }
 
